2ce6e046a3c92583085211f6122549746d2c6ca3
LeadHood <adrianwallin07@gmail.com>
2026-07-05 02:53:45 +0200

    Added kitty and hypr

---
+++ scripts/scripts/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+pacman -Syu
+pacman -S --needed --noconfirm git nvim hyprpolkitagent firefox ranger hyprpaper xdg-desktop-portal-hyprland waybar wl-clipboard nsxiv brightnessctl npm base-devel github-cli
